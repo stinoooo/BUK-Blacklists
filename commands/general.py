7bde42bbe34e0201b65db7a93054f016ea060898
stino <stabbajabba@gmail.com>
2024-10-09 17:47:41 +0200

    Add files via upload

--- commands/general.py
+++ commands/general.py
@@ -1,6 +1,14 @@
 import discord
 from discord import app_commands
 from discord.ext import commands
+from utils.checks import is_moderation_or_admin
+
+# Define or import the role IDs at the top of your file
+MOD_TEAM_1 = 1236358475827904664  # Replace with actual ID
+ADMIN_TEAM_1 = 1225934070794555455  # Replace with actual ID
+DEV_USER_ID = 186117507554344960  # Bot developer's user ID
+MOD_TEAM_2 = 1236379144141541489  # Replace with actual ID
+ADMIN_TEAM_2 = 1236379225565433927  # Replace with actual ID
 
 class General(commands.Cog):
     def __init__(self, bot):
@@ -10,7 +18,6 @@ class General(commands.Cog):
     async def help_command(self, interaction: discord.Interaction):
         embed = discord.Embed(
             title="Help - Available Commands",
-            description="Here are the commands you have access to:",
             color=discord.Color(0x013a93)
         )
 
@@ -81,13 +88,13 @@ class General(commands.Cog):
 
         # Check user's roles and add relevant commands to the embed
         if any(role.id in [MOD_TEAM_1, MOD_TEAM_2] for role in interaction.user.roles):
-            embed.add_field(name="Moderation Commands", value="\n".join([f"**{cmd['name']}** - {cmd['description']}" for cmd in commands_list["Moderation Commands"]]), inline=False)
+            embed.add_field(name="Moderation Commands", value="\n".join([f"{cmd['name']} - {cmd['description']}" for cmd in commands_list["Moderation Commands"]]), inline=False)
 
         if any(role.id in [ADMIN_TEAM_1, ADMIN_TEAM_2] for role in interaction.user.roles):
-            embed.add_field(name="Admin Commands", value="\n".join([f"**{cmd['name']}** - {cmd['description']}" for cmd in commands_list["Admin Commands"]]), inline=False)
+            embed.add_field(name="Admin Commands", value="\n".join([f"{cmd['name']} - {cmd['description']}" for cmd in commands_list["Admin Commands"]]), inline=False)
 
         if interaction.user.id == DEV_USER_ID:  # Assuming DEV_USER_ID is defined elsewhere
-            embed.add_field(name="Bot Developer Commands", value="\n".join([f"**{cmd['name']}** - {cmd['description']}" for cmd in commands_list["Bot Developer Commands"]]), inline=False)
+            embed.add_field(name="Bot Developer Commands", value="\n".join([f"{cmd['name']} - {cmd['description']}" for cmd in commands_list["Bot Developer Commands"]]), inline=False)
 
         await interaction.response.send_message(embed=embed)
 
