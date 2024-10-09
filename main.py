ecd196fe95646bcdad3a19ba2295b2a95cbc9fd0
stino <stabbajabba@gmail.com>
2024-10-09 14:03:27 +0200

    [FEAT] Global Slash Command Sync & Handling DM's
    - In the on_ready event, the bot will now sync slash commands globally by calling bot.tree.sync()
    - In the on_message event, the bot checks if the message is from a direct message (DM) channel.
    - If it is a DM, the bot sends an embed explaining that it cannot read or respond to messages. The embed includes a button that, when clicked, redirects the user to the BUK modmail bot.

--- main.py
+++ main.py
@@ -27,16 +27,33 @@ async def on_ready():
     print(f'{bot.user} is online!')
     await bot.change_presence(activity=discord.Game(name="Managing Blacklists"))
 
-    # Sync slash commands for a specific guild for faster testing
-    guild_id = os.getenv('GUILD_ID')  # Set the GUILD_ID in your .env file for testing
-    if guild_id:
-        guild = discord.Object(id=int(guild_id))
-        synced = await bot.tree.sync(guild=guild)
-        print(f"Synced {len(synced)} slash commands to guild {guild_id}.")
-    else:
-        # Global sync if no GUILD_ID is provided
-        synced = await bot.tree.sync()
-        print(f"Synced {len(synced)} slash commands globally.")
+    # Sync globally
+    synced_commands = await bot.tree.sync()
+    print(f"Global slash commands synced: {len(synced_commands)} commands available.")
+
+@bot.event
+async def on_message(message):
+    # Ignore messages from the bot itself
+    if message.author == bot.user:
+        return
+    
+    # Check if it's a DM
+    if isinstance(message.channel, discord.DMChannel):
+        # Create an embed
+        embed = discord.Embed(
+            title="Direct Messages Not Supported",
+            description="We are unable to respond to direct messages sent to this bot. If you need assistance or wish to contact the BUK Moderation Team, "
+                        "please feel free to reach out to them directly via Modmail by clicking the button below.",
+            color=discord.Color.blue()
+        )
+        
+        # Create a button that leads to the modmail channel
+        view = discord.ui.View()
+        button = discord.ui.Button(label="Contact BUK Modmail", url="https://discordapp.com/channels/@me/1282748337711353927", style=discord.ButtonStyle.link)
+        view.add_item(button)
+        
+        # Send the embed and button as a response
+        await message.channel.send(embed=embed, view=view)
 
 # Main entry point
 async def main():
